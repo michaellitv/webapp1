d67b901791da8a3edfb1f097be132fff56a2be3e
michaellitv <42909784+michaellitv@users.noreply.github.com>
2018-10-06 00:39:35 +0300

    added Serilog package

--- WebApp1/Controllers/ValuesController.cs
+++ WebApp1/Controllers/ValuesController.cs
@@ -4,14 +4,37 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Serilog;
+using Serilog.Core;
+using Serilog.Sinks.Fluentd;
 
 namespace WebApp1.Controllers
 {
 	public class ValuesController : ApiController
 	{
+		public ValuesController()
+		{
+			Log.Logger = new LoggerConfiguration().CreateLogger();
+		}
+
 		// GET api/values
 		public IEnumerable<string> Get()
 		{
+			//var log = new LoggerConfiguration()
+			//    .WriteTo.Fluentd()
+			//    .CreateLogger();
+
+
+			/*var log = new LoggerConfiguration()
+                .WriteTo.Fluentd(new FluentdHandlerSettings
+                {
+                    Tag = "My.SampleApp"
+                })
+                .CreateLogger();*/
+
+			Log.Information("No one listens to me!");
+
+
 			return new string[] { "value1", "value2" };
 		}
 
